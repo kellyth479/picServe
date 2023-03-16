133349d66a6a1dde4f40dc3ae23c2930df95fc6e
LiquidGangster <thomasjpkelly@gmail.com>
2023-03-15 20:25:00 -0400

    serve first pic

--- src/main/java/com/picServe/APIs/GreetController.java
+++ src/main/java/com/picServe/APIs/GreetController.java
@@ -1,6 +1,12 @@
 package com.picServe.APIs;
 
+import java.io.InputStream;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +24,14 @@ public class GreetController {
     @GetMapping("/face")
     public String error(){
         return "face";
+    }
+    @GetMapping("/italyImage")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> getItalyImage() {
+        MediaType contentType =  MediaType.IMAGE_PNG;
+        InputStream in =  getClass().getResourceAsStream("/com/picServe/images/venice.png");
+        return ResponseEntity.ok()
+          .contentType(contentType)
+          .body(new InputStreamResource(in));
     }
 }
